ea1195fe5e2e34b05eab584cb7c1cc9b581df727
Ahmadzai Nasery <60744269+Sliferss@users.noreply.github.com>
2024-07-20 12:21:57 +0100

    commit

--- map/views.py
+++ map/views.py
@@ -25,25 +25,39 @@ class PrefabsView(View):
         if not is_ajax(request):
             context["prefabs"] = self.queryset
             return render(request, self.template_name, context)
-        name = request.GET.get("name")
-        if not name:
-            context["error"] = True
-            return JsonResponse(context)
+
         type = request.GET.get("type")
+        if type == "modal_get":
+            context, name = self.name_check(request, context)
+            prefab_obj = PrefabsConveyor.objects.filter(name=name)
+            if not prefab_obj:
+                context["error"] = "Prefab Doesn't Exist!"
+                return JsonResponse(context)
+            context["prefab"] = list(prefab_obj.values())
+            return JsonResponse(context)
         if type == "update":
+            context, name = self.name_check(request, context)
             prefab_obj = PrefabsConveyor.objects.filter(name=name).first()
             if not prefab_obj:
                 context["error"] = "Prefab Doesn't Exist!"
                 return JsonResponse(context)
-            context = self.handle_update(request, context, prefab_obj)
+            context = self.handle_update(request, prefab_obj)
         if type == "create":
+            context, name = self.name_check(request, context)
             prefab_obj = PrefabsConveyor.objects.filter(name=name).first()
             if prefab_obj:
                 context["error"] = "Prefab Name Exist!"
                 return JsonResponse(context)
+            self.handle_create(request)
 
-    def handle_update(self, request, context, prefab_obj):
+    def name_check(self, request, context):
+        name = request.GET.get("name")
+        if not name:
+            context["error"] = True
+        return context, name
+
+    def handle_update(self, request, prefab_obj):
         pass
 
-    def handle_create(self, request, context, prefab_obj):
+    def handle_create(self, request):
         pass
