3cd8b5979fd853392ab168496e2c5dd8c2450b7c
Ahmadzai Nasery <60744269+Sliferss@users.noreply.github.com>
2024-07-20 13:14:17 +0100

    commit

--- map/views.py
+++ map/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.generic import View
 from .models import PrefabsConveyor, MapSetup, GridParts
+from django.utils.crypto import get_random_string
+from .models import PrefabsConveyor
+import os
+from django.conf import settings
 
 def is_ajax(request):
     return request.headers.get("x-requested-with") == "XMLHttpRequest"
@@ -18,10 +22,26 @@ class Index(View):
 
 class PrefabsView(View):
     template_name = "prefabs_view.html"
-    queryset = PrefabsConveyor.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        context = {}
+        context["error"] = False
+        self.name = request.POST.get('name')
+        if not self.name:
+            context["error"] = "Missing Name!"
+            return JsonResponse(context)
+        self.type = request.POST.get('type')
+        self.length = request.POST.get('length')
+        self.width = request.POST.get('width')
+        self.height = request.POST.get('height')
+        self.image = request.FILES.get('image')
+        if self.type == "create":
+            context = self.handle_create(context)
+        return JsonResponse(context)
 
     def get(self, request, *args, **kwargs):
         context = {}
+        self.queryset = PrefabsConveyor.objects.all()
         if not is_ajax(request):
             context["prefabs"] = self.queryset
             return render(request, self.template_name, context)
@@ -42,13 +62,6 @@ class PrefabsView(View):
                 context["error"] = "Prefab Doesn't Exist!"
                 return JsonResponse(context)
             context = self.handle_update(request, prefab_obj)
-        if type == "create":
-            context, name = self.name_check(request, context)
-            prefab_obj = PrefabsConveyor.objects.filter(name=name).first()
-            if prefab_obj:
-                context["error"] = "Prefab Name Exist!"
-                return JsonResponse(context)
-            self.handle_create(request)
 
     def name_check(self, request, context):
         name = request.GET.get("name")
@@ -59,5 +72,31 @@ class PrefabsView(View):
     def handle_update(self, request, prefab_obj):
         pass
 
-    def handle_create(self, request):
-        pass
+    def handle_create(self, context):
+        if PrefabsConveyor.objects.filter(name=self.name):
+            context["error"] = "Name Already Exists!"
+            return context
+        create_obj = PrefabsConveyor.objects.create(name=self.name)
+        if self.length:
+            create_obj.length = self.length
+        if self.width:
+            create_obj.width = self.width
+        if self.height:
+            create_obj.height = self.height
+        if self.image:
+            image_name = self.image.name
+            image_path = os.path.join(settings.MEDIA_ROOT, image_name)
+            if os.path.exists(image_path):
+                base, ext = os.path.splitext(image_name)
+                unique_id = get_random_string(6)
+                image_name = f"{base}_{unique_id}{ext}"
+                image_path = os.path.join(settings.MEDIA_ROOT, image_name)
+            with open(image_path, 'wb+') as destination:
+                for chunk in self.image.chunks():
+                    destination.write(chunk)
+            print(image_path)
+            create_obj.image = image_path
+            create_obj.save()
+        create_obj.save()
+        context["success"] = "Created Prefab: " + self.name
+        return context
