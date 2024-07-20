4ec79a56c42de65af884d42c6cb2fe6b63cab7b5
Ahmadzai Nasery <60744269+Sliferss@users.noreply.github.com>
2024-07-20 11:39:45 +0100

    commit

--- map/views.py
+++ map/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from django.views.generic import View
 from .models import PrefabsConveyor, MapSetup, GridParts
 
@@ -24,3 +25,25 @@ class PrefabsView(View):
         if not is_ajax(request):
             context["prefabs"] = self.queryset
             return render(request, self.template_name, context)
+        name = request.GET.get("name")
+        if not name:
+            context["error"] = True
+            return JsonResponse(context)
+        type = request.GET.get("type")
+        if type == "update":
+            prefab_obj = PrefabsConveyor.objects.filter(name=name).first()
+            if not prefab_obj:
+                context["error"] = "Prefab Doesn't Exist!"
+                return JsonResponse(context)
+            context = self.handle_update(request, context, prefab_obj)
+        if type == "create":
+            prefab_obj = PrefabsConveyor.objects.filter(name=name).first()
+            if prefab_obj:
+                context["error"] = "Prefab Name Exist!"
+                return JsonResponse(context)
+
+    def handle_update(self, request, context, prefab_obj):
+        pass
+
+    def handle_create(self, request, context, prefab_obj):
+        pass
