ff02711463eed49bde6a620548a676746ddc63f1
Ahmadzai Nasery <60744269+Sliferss@users.noreply.github.com>
2024-08-04 13:08:45 +0100

    commit

--- map/admin.py
+++ map/admin.py
@@ -3,12 +3,12 @@ from .models import PrefabsConveyor, GridParts, MapSetup
 
 @admin.register(PrefabsConveyor)
 class PrefabsConveyorAdmin(admin.ModelAdmin):
-    list_display = ('name', 'length', 'width', 'height', 'speed')
+    list_display = ('name', 'image')
 
 @admin.register(GridParts)
 class GridPartsAdmin(admin.ModelAdmin):
-    list_display = ('name', 'map', 'length', 'width', 'height', 'speed')
+    list_display = ('name', 'map', 'position_x', 'position_y')
 
 @admin.register(MapSetup)
 class MapSetupAdmin(admin.ModelAdmin):
-    list_display = ('name', 'grid_width', 'grid_height')
+    list_display = ('name', 'grid_width', 'grid_height', 'image')
